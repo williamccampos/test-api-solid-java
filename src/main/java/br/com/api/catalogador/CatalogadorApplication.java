852b9256453a0a2e13d7f1221b108dede0c04392
William Campos <william.c.campos@icloud.com>
2022-05-07 10:41:54 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/api/catalogador/CatalogadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.catalogador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogadorApplication.class, args);
+	}
+
+}
